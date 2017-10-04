9c7e1cfdf390b8b4b15f06217a40a31647dadacb
Chris Frye <cfrye2913@gmail.com>
2017-10-04 09:30:28 -0400

    Button Click

--- app/src/main/java/com/example/student/cooper_assign2/MainActivity.java
+++ app/src/main/java/com/example/student/cooper_assign2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.student.cooper_assign2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,11 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get references to elements on the activity
+        Button btnTeachers = (Button)findViewById(R.id.btnTeachers);
+        Button btnStudents = (Button)findViewById(R.id.btnStudents);
+
+
+        //Button click handler for btnTeacher
+        btnTeachers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TeacherListActivity.class));
+            }
+        });
     }
 
-    //button click handler for the teachers button
-    //this button opens the teacherlist activity
-    public void moveToTeacher(){
-        startActivity(new Intent(MainActivity.this, TeacherListActivity.class));
-    }
 }
